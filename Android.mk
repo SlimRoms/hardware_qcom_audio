f5f4e6f64096d435fdb27e487cae39be3d433ebf
cordell <cordellsmail@gmail.com>
2013-09-03 05:33:51 -0500

    match CM

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
-ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 ifeq ($(TARGET_QCOM_AUDIO_VARIANT),)
+
+ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
 
