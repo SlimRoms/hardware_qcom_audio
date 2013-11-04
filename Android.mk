2d5a7dab2c374b8c0d24217afebb170bc933a09a
Nicholas Flintham <nick@flinny.org>
2013-11-04 21:19:01 +0000

    Audio: Build barrier for AUDIO_VARIANT
    Change-Id: I4097a02bf6b49efe6415b9b85fcd84fede09dba7

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_QCOM_AUDIO_VARIANT),)
+
 ifneq ($(filter msm8960 msm8226 msm8x26 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
@@ -10,4 +12,5 @@ include $(MY_LOCAL_PATH)/voice_processing/Android.mk
 include $(MY_LOCAL_PATH)/visualizer/Android.mk
 endif
 
+endif
 endif
