ef2b78b1ee99ebe77eebad858e567daf5dc3d79c
Ivan Grinko <iivanich@gmail.com>
2015-01-25 10:14:38 +0200

    msm8660: Remove unused code

--- Android.mk
+++ Android.mk
@@ -1,16 +1,11 @@
 ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
 
-ifneq ($(filter msm8960 msm8226 msm8x26 msm8660 msm8974 msm8x74 msm8x84 msm8084,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
 
 MY_LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
 include $(MY_LOCAL_PATH)/legacy/Android.mk
-else
-include $(MY_LOCAL_PATH)/hal/Android.mk
-include $(MY_LOCAL_PATH)/voice_processing/Android.mk
-include $(MY_LOCAL_PATH)/visualizer/Android.mk
-include $(MY_LOCAL_PATH)/post_proc/Android.mk
 endif
 
 endif
