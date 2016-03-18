16f22f250a04be2215385e86706d8d30a12f0d14
Ivan Grinko <iivanich@gmail.com>
2016-03-18 09:44:14 +0200

    policy_hal: Add custom audio policy
    For now this is equal to the system wide policy manager, except
    for added support for FM device loopback. Which will give
    working playback and recording with the pending updates to
    QCOM's FM app.
    https://github.com/CyanogenMod/android_hardware_qcom_audio/commit/fb3b822e3e5f044969629d75b43d4865943a35c9

--- legacy/Android.mk
+++ legacy/Android.mk
@@ -3,4 +3,5 @@ AUDIO_HW_ROOT := $(call my-dir)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
     include $(AUDIO_HW_ROOT)/msm8660/Android.mk
     include $(AUDIO_HW_ROOT)/mm-audio/Android.mk
+    include $(AUDIO_HW_ROOT)/policy_hal/Android.mk
 endif
