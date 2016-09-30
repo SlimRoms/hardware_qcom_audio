a9eccb63f39f36349ebdd782bdb46deb4d6b28d5
Divya Narayanan Poojary <dnaray@codeaurora.org>
2016-09-30 18:52:13 +0530

    hal: Restrict SSR recording to channel position representation
    Surround Sound Recording(SSR) supports only channel position
    representation while default channel mask for multi-channel
    recording is always index representation.
    Return updated channel mask in case index representation is sent
    for channel count 6. This ensures audio flinger reopens the session
    with updated position representation channel mask.
    And also handled the case if any client requests recording with
    multichannel when SSR is not enabled, will check the max_mic_count
    supported by the device and update the channel mask accordingly.
    
    Change-Id: Ib925a5f31d50182cd595e547f3412ed809224a5a

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -4445,3 +4445,8 @@ int platform_set_snd_device_name(snd_device_t device, const char *name)
 done:
     return ret;
 }
+
+int platform_get_max_mic_count(void *platform) {
+    struct platform_data *my_data = (struct platform_data *)platform;
+    return my_data->max_mic_count;
+}
