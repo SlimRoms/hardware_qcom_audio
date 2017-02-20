0e6538592c996135d4f90cb27a64ab4085460da2
umang96 <umangleekha3@gmail.com>
2017-02-20 22:35:31 +0530

    Merge remote-tracking branch 'los/cm-14.1-caf-8952' into ng7.1-caf-8952
    * To bring exactly in line with lineage
    
    Change-Id: Ia930311dadb4c1e39acb60421d1ff386499cd17f

--- hal/msm8974/platform.c
+++ hal/msm8974/platform.c
@@ -4435,6 +4435,11 @@ int platform_get_wsa_mode (void *adev)
     return 0;
 }
 
+int platform_get_max_mic_count(void *platform) {
+    struct platform_data *my_data = (struct platform_data *)platform;
+    return my_data->max_mic_count;
+}
+
 int platform_set_snd_device_name(snd_device_t device, const char *name)
 {
     int ret = 0;
@@ -4448,9 +4453,4 @@ int platform_set_snd_device_name(snd_device_t device, const char *name)
     device_table[device] = strdup(name);
 done:
     return ret;
-}
-
-int platform_get_max_mic_count(void *platform) {
-    struct platform_data *my_data = (struct platform_data *)platform;
-    return my_data->max_mic_count;
 }
